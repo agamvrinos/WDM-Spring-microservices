99b7ac60fb8d7e592234116165431857f8ec9c7d
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-05-09 14:12:30 +0200

    Implement Payments microservice

---
+++ wdm-microservices-project/payments/src/main/java/wdm/project/enums/Status.java
@@ -0,0 +1,26 @@
+package wdm.project.enums;
+
+public enum Status {
+    SUCCESS("SUCCESS"),
+    FAILURE("FAILURE"),
+    PENDING("PENDING");
+
+    private String value;
+
+	Status(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+        return value;
+    }
+
+    public static Status findStatusEnum(String status) {
+		for (Status each : Status.values()) {
+			if (each.value.equals(status)) {
+				return each;
+			}
+		}
+		return null;
+    }
+}
