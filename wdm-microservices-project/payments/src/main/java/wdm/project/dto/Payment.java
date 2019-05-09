f1dc5019169c9ddafcf5b5dcdd342a8a0ab50684
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-05-09 14:12:30 +0200

    Implement Payments microservice

--- wdm-microservices-project/payments/src/main/java/wdm/project/dto/Payment.java
+++ wdm-microservices-project/payments/src/main/java/wdm/project/dto/Payment.java
@@ -1,8 +1,110 @@
 package wdm.project.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "payment", schema = "public")
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "status", length = 100)
+    private String status;
+
+    public Payment() {
+    }
+
+	/**
+	 * Gets id.
+	 *
+	 * @return Value of id.
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Sets new id.
+	 *
+	 * @param id New value of id.
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+     * Gets userId.
+     *
+     * @return Value of userId.
+     */
+    public Long getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets new userId.
+     *
+     * @param userId New value of userId.
+     */
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets orderId.
+     *
+     * @return Value of orderId.
+     */
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * Sets new orderId.
+     *
+     * @param orderId New value of orderId.
+     */
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * Gets status.
+     *
+     * @return Value of status.
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets new status.
+     *
+     * @param status New value of status.
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+               "orderId=" + orderId +
+               ", userId=" + userId +
+               ", status='" + status + '\'' +
+               '}';
+    }
 }
--- wdm-microservices-project/payments/src/main/java/wdm/project/endpoint/PaymentsEndpoint.java
+++ wdm-microservices-project/payments/src/main/java/wdm/project/endpoint/PaymentsEndpoint.java
@@ -1,5 +1,42 @@
 package wdm.project.endpoint;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wdm.project.dto.Payment;
+import wdm.project.service.PaymentsService;
+
+@RestController
+@RequestMapping("/payments")
 public class PaymentsEndpoint {
 
+    @Autowired
+    private PaymentsService paymentsService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping("/status/{order_id}")
+    public JsonNode getPaymentStatus(@PathVariable("order_id") Long orderId) {
+        Payment payment = paymentsService.getPaymentByOrderId(orderId);
+        return objectMapper.createObjectNode().put("status", payment.getStatus());
+    }
+
+    @PostMapping("/pay/{order_id}")
+    public JsonNode payOrder(
+            @PathVariable("order_id") Long orderId,
+            @RequestParam("userId") Long userId,
+            @RequestParam("total") Long total) {
+        if (userId == null || total == null) {
+            throw new IllegalArgumentException("Params were not provided");
+        }
+        Payment payment = paymentsService.payOrder(orderId, userId, total);
+        return objectMapper.createObjectNode().put("status", payment.getStatus());
+    }
 }
