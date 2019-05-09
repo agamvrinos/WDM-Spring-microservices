06350d2f16f4cc93754e5ef63c032914986b9969
Panagiotis Soilis <pan.soilis@gmail.com>
2019-05-09 14:56:45 +0200

    Updated insert statements

--- wdm-microservices-project/payments/src/main/resources/data.sql
+++ wdm-microservices-project/payments/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 --
--- Data for Name: Payment; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: payment; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public."Payment" (user_id, order_id, status) VALUES (2, 4, 'success');
-INSERT INTO public."Payment" (user_id, order_id, status) VALUES (1, 7, 'failure');
-INSERT INTO public."Payment" (user_id, order_id, status) VALUES (3, 2, 'success');
-INSERT INTO public."Payment" (user_id, order_id, status) VALUES (4, 1, 'pending');
+INSERT INTO public."payment" (user_id, order_id, status) VALUES (2, 4, 'success');
+INSERT INTO public."payment" (user_id, order_id, status) VALUES (1, 7, 'failure');
+INSERT INTO public."payment" (user_id, order_id, status) VALUES (3, 2, 'success');
+INSERT INTO public."payment" (user_id, order_id, status) VALUES (4, 1, 'pending');
 
 
 --
