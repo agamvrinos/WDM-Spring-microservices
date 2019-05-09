fa3a215f8534d803abc9f0c58c4f95713dde8ccd
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-05-09 13:06:07 +0200

    Change Payments database schema

--- wdm-microservices-project/payments/src/main/resources/data.sql
+++ wdm-microservices-project/payments/src/main/resources/data.sql
@@ -2,10 +2,10 @@
 -- Data for Name: Payment; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public."Payment" (id, user_id, order_id, status) VALUES (1, 2, 4, 'success');
-INSERT INTO public."Payment" (id, user_id, order_id, status) VALUES (2, 1, 7, 'failure');
-INSERT INTO public."Payment" (id, user_id, order_id, status) VALUES (3, 3, 2, 'success');
-INSERT INTO public."Payment" (id, user_id, order_id, status) VALUES (4, 4, 1, 'pending');
+INSERT INTO public."Payment" (user_id, order_id, status) VALUES (2, 4, 'success');
+INSERT INTO public."Payment" (user_id, order_id, status) VALUES (1, 7, 'failure');
+INSERT INTO public."Payment" (user_id, order_id, status) VALUES (3, 2, 'success');
+INSERT INTO public."Payment" (user_id, order_id, status) VALUES (4, 1, 'pending');
 
 
 --
