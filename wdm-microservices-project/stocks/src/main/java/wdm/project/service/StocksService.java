3d360ae71c42ecc98d32d2909b6d25d17f66cc77
agamvrinos <agg_gam@hotmail.com>
2019-05-14 02:04:48 +0200

    Stock updates

--- wdm-microservices-project/stocks/src/main/java/wdm/project/service/StocksService.java
+++ wdm-microservices-project/stocks/src/main/java/wdm/project/service/StocksService.java
@@ -11,45 +11,79 @@ public class StocksService {
     @Autowired
     private StocksRepository stocksRepository;
 
+    /**
+     * Returns the Item instance with the provided id.
+     *
+     * @param itemId the id of the instance to be fetched
+     * @return the Item with the provided id
+     */
     public Item getItem(Long itemId) {
-        Boolean exists = stocksRepository.existsById(itemId);
-        if (!exists) {
-            throw new RuntimeException();
+        if (itemId == null) {
+            throw new RuntimeException("Id was not provided");
+        }
+        boolean existsStocks = stocksRepository.existsById(itemId);
+        if (!existsStocks) {
+            throw new RuntimeException("There was no stock with the provided id");
         }
         return stocksRepository.findById(itemId).orElseThrow(RuntimeException::new);
     }
 
+    /**
+     * Returns the availability of the Item with the
+     * provided id.
+     *
+     * @param itemId the id of the item
+     * @return the stock of the item
+     */
     public Integer getItemAvailability(Long itemId) {
-        Boolean exists = stocksRepository.existsById(itemId);
-        if (!exists) {
-            throw new RuntimeException();
-        }
-        Item item = stocksRepository.findById(itemId).orElseThrow(RuntimeException::new);
+        Item item = getItem(itemId);
         return item.getStock();
     }
 
+    /**
+     * Creates a new Item based on the provided
+     * {@code requestItem}.
+     *
+     * @param requestItem the request item
+     * @return the id of the stored item
+     */
     public Long createItem(Item requestItem) {
         Item item = new Item();
         item.setTitle(requestItem.getTitle());
         item.setStock(requestItem.getStock());
         item.setPrice(requestItem.getPrice());
-        stocksRepository.save(item);
-        return item.getId();
+        Item storedItem = stocksRepository.save(item);
+        return storedItem.getId();
     }
 
-    public void addItem(Long itemId, Integer itemNumber) {
-        Item item = stocksRepository.findById(itemId).orElseThrow(RuntimeException::new);
-        item.setStock(item.getStock() + itemNumber);
+    /**
+     * Adds stock to the Item instance with the provided id.
+     *
+     * @param itemId the id of the Item, the stock of which is
+     * is goings to be updated
+     * @param additionalStock the stock to be added
+     */
+    public void addItem(Long itemId, Integer additionalStock) {
+        Item item = getItem(itemId);
+        Integer currentStock = item.getStock();
+        item.setStock(currentStock + additionalStock);
         stocksRepository.save(item);
     }
 
-    public void subtractItem(Long itemId, Integer itemNumber) {
-        Item item = stocksRepository.findById(itemId).orElseThrow(RuntimeException::new);
-        Integer updatedStock = item.getStock() - itemNumber;
-        if (updatedStock < 0) {
-            throw new RuntimeException();
+    /**
+     * Subtracts stock from the Item instance with the provided id.
+     *
+     * @param itemId the id of the Item, the stock of which is
+     *      * is goings to be updated
+     * @param subtractedStock the stock to be subtracted
+     */
+    public void subtractItem(Long itemId, Integer subtractedStock) {
+        Item item = getItem(itemId);
+        Integer currentStock = item.getStock();
+        if (subtractedStock > currentStock) {
+            throw new RuntimeException("Stock cannot be negative");
         }
-        item.setStock(item.getStock() - itemNumber);
+        item.setStock(currentStock - subtractedStock);
         stocksRepository.save(item);
     }
 }
