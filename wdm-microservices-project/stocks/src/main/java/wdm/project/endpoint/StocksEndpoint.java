230c9e63b1b80a3463e3acbaa0105ca503059476
psoilis <34517649+psoilis@users.noreply.github.com>
2019-05-14 02:08:24 +0200

    Implement Stocks microservice

--- wdm-microservices-project/stocks/src/main/java/wdm/project/endpoint/StocksEndpoint.java
+++ wdm-microservices-project/stocks/src/main/java/wdm/project/endpoint/StocksEndpoint.java
@@ -1,5 +1,39 @@
 package wdm.project.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import wdm.project.dto.Item;
+import wdm.project.service.StocksService;
+
+@RestController
+@RequestMapping("/stock")
 public class StocksEndpoint {
 
+    @Autowired
+    private StocksService stocksService;
+
+	@GetMapping("/item/{id}")
+    public Item getItem(@PathVariable("id") Long itemId) {
+        return stocksService.getItem(itemId);
+    }
+
+    @GetMapping("/availability/{id}")
+    public Integer getItemAvailability(@PathVariable("id") Long itemId) {
+        return stocksService.getItemAvailability(itemId);
+    }
+
+    @PostMapping("/item/create/")
+    public Long createItem(@RequestBody Item requestItem) {
+        return stocksService.createItem(requestItem);
+    }
+
+    @PostMapping("/add/{item_id}/{number}")
+    public void addItem(@PathVariable("item_id") Long itemId, @PathVariable("number") Integer itemNumber) {
+        stocksService.addItem(itemId, itemNumber);
+    }
+
+    @PostMapping("/subtract/{item_id}/{number}")
+    public void subtractItem(@PathVariable("item_id") Long itemId, @PathVariable("number") Integer itemNumber) {
+        stocksService.subtractItem(itemId, itemNumber);
+    }
 }
