189b6486567a6f83910064f3e1ce121f158000e0
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-05-15 11:15:33 +0200

    Payments-ms: Add custom exception handling

--- wdm-microservices-project/payments/src/main/java/wdm/project/endpoint/PaymentsEndpoint.java
+++ wdm-microservices-project/payments/src/main/java/wdm/project/endpoint/PaymentsEndpoint.java
@@ -3,6 +3,7 @@ package wdm.project.endpoint;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wdm.project.dto.Payment;
 import wdm.project.enums.Status;
+import wdm.project.exception.PaymentException;
 import wdm.project.service.PaymentsService;
 
 @RestController
@@ -23,7 +25,7 @@ public class PaymentsEndpoint {
     private ObjectMapper objectMapper;
 
     @GetMapping("/status/{order_id}")
-    public JsonNode getPaymentStatus(@PathVariable("order_id") Long orderId) {
+    public JsonNode getPaymentStatus(@PathVariable("order_id") Long orderId) throws PaymentException {
         Payment payment = paymentsService.getPaymentByOrderId(orderId);
         return objectMapper.createObjectNode().put("status", payment.getStatus());
     }
@@ -32,15 +34,15 @@ public class PaymentsEndpoint {
     public void payOrder(
             @PathVariable("order_id") Long orderId,
             @PathVariable("user_id") Long userId,
-            @PathVariable("total") Integer total) {
+            @PathVariable("total") Integer total
+    ) throws PaymentException {
         if (userId == null || total == null) {
-            throw new IllegalArgumentException("Params were not provided");
+            throw new PaymentException("Params were not provided", HttpStatus.BAD_REQUEST);
         }
         Payment payment = paymentsService.payOrder(orderId, userId, total);
         Status statusEnum = Status.findStatusEnum(payment.getStatus());
         if (statusEnum != Status.SUCCESS) {
-            // TODO: Return 400
-            throw new RuntimeException();
+            throw new PaymentException("Payment status is not \"SUCCESS\"", HttpStatus.BAD_REQUEST);
         }
     }
 }
