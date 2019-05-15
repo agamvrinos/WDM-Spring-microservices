9d4807a17bcd6f5bdb0a17b182e6a55ba15fe4cc
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-05-15 11:15:33 +0200

    Payments-ms: Add custom exception handling

---
+++ wdm-microservices-project/payments/src/main/java/wdm/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package wdm.project.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(PaymentException.class)
+    public ResponseEntity<ExceptionResponse> handleControllerError(HttpServletRequest req, PaymentException ex) {
+        ExceptionResponse response = new ExceptionResponse(ex.getErrorMessage(), ex.getHttpStatus());
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+}
