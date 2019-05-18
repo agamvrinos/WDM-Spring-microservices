49551062e20e82e87939a072bbbb1d77c19b2424
agamvrinos <agg_gam@hotmail.com>
2019-05-18 17:26:07 +0200

    Add Discovery service & Basic ms config
    - Add Eureka Server
    - Add ms application names
    - Update ms maven dependencies
    - Add eureka client configuration properties
    - Setup Eureka clients

--- wdm-microservices-project/eureka/src/main/java/wdm/project/EurekaRegistrationServer.java
+++ wdm-microservices-project/eureka/src/main/java/wdm/project/EurekaRegistrationServer.java
@@ -2,13 +2,13 @@ package wdm.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class StocksApplication {
+@EnableEurekaServer
+public class EurekaRegistrationServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StocksApplication.class, args);
+		SpringApplication.run(EurekaRegistrationServer.class, args);
 	}
 }
