52df938a6f1346f1d1ec2bdaf2b8732af8730b1c
jannes <jannes.dev@posteo.net>
2019-05-19 17:22:24 +0200

    add dockerfile for config service, update global build file

--- wdm-microservices-project/build.sh
+++ wdm-microservices-project/build.sh
@@ -1,4 +1,18 @@
+cd config-service
 mvn package
+cd -
+cd orders
+mvn package
+cd -
+cd payments
+mvn package
+cd -
+cd stocks
+mvn package
+cd -
+cd users
+mvn package
+cd -
 docker build -t wdm/users users/
 docker build -t wdm/stocks stocks/
 docker build -t wdm/payments payments/
