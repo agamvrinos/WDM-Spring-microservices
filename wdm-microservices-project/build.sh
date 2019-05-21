ec0cc3fb0518245103656a88feccdfb9c512b706
jannes <jannes.dev@posteo.net>
2019-05-21 11:37:15 +0200

    add dockerfile eureka and include in compose file

--- wdm-microservices-project/build.sh
+++ wdm-microservices-project/build.sh
@@ -13,8 +13,12 @@ cd -
 cd users
 mvn package
 cd -
+cd eureka
+mvn package
+cd -
 docker build -t wdm/users users/
 docker build -t wdm/stocks stocks/
 docker build -t wdm/payments payments/
 docker build -t wdm/orders orders/
 docker build -t wdm/config config-service/
+docker build -t wdm/eureka eureka
