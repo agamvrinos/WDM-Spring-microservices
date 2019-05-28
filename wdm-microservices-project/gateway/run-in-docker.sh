6782a1de6e83f558e359405c815d99d6e55bfde5
jannes <jannes.dev@posteo.net>
2019-05-28 17:07:06 +0200

    postgres-proj: add dockerfile and runscript for gateway server

---
+++ wdm-microservices-project/gateway/run-in-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the configuration server to start on port 8888"
+echo "********************************************************"
+while ! `nc -z config-service 8888`; do sleep 3; done
+echo "*******  Configuration Server has started"
+
+
+echo "********************************************************"
+echo "Waiting for the eureka server to start on port 8761"
+echo "********************************************************"
+while ! `nc -z eureka-service 8761`; do sleep 3; done
+echo "******* Eureka Server has started"
+
+
+echo "********************************************************"
+echo "Starting Gateway Service with Configuration Service via Eureka :  $EUREKA_URI"
+echo "Using Profile: $PROFILE"
+echo "********************************************************"
+java -Dserver.port=8086   \
+-Dspring.profiles.active=$PROFILE -jar /app.jar
