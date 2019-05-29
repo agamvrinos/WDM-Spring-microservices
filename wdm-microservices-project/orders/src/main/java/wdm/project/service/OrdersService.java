8c2d456bfabe4eb98d855d94472d31985781d621
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-05-29 19:39:09 +0200

    Postgres-proj: Add rollback of stock
    In this commit, the addItems method is added to the stock service, as
    well as using this method to roll back any change in stock when the
    payment of a checkout has failed.

--- wdm-microservices-project/orders/src/main/java/wdm/project/service/OrdersService.java
+++ wdm-microservices-project/orders/src/main/java/wdm/project/service/OrdersService.java
@@ -1,5 +1,6 @@
 package wdm.project.service;
 
+import feign.Feign;
 import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -177,14 +178,24 @@ public class OrdersService {
      */
     public void checkoutOrder(Long orderId) throws OrderException {
         OrdersWrapper order = findOrder(orderId);
+        Integer price;
         try {
-            Integer price = stocksServiceClient.subtractItems(order.getOrderItems());
-            paymentsServiceClient.payOrder(orderId, order.getUserId(), price);
+            price = stocksServiceClient.subtractItems(order.getOrderItems());
         } catch (FeignException exception) {
             if (exception.status() == 400) {
                 throw new OrderException(new String(exception.content()), HttpStatus.BAD_REQUEST);
             } else {
-                throw new OrderException("Something went wrong when handling the checkout", HttpStatus.INTERNAL_SERVER_ERROR);
+                throw new OrderException("Something went wrong when subtracting the stock.", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        try {
+            paymentsServiceClient.payOrder(orderId, order.getUserId(), price);
+        } catch (FeignException exception) {
+            if (exception.status() == 400) {
+                stocksServiceClient.addItems(order.getOrderItems());
+                throw new OrderException(new String(exception.content()), HttpStatus.BAD_REQUEST);
+            } else {
+                throw new OrderException("Something went wrong when paying the order.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
     }
