a8abb7132bb890c9b636cf24b6eac81bf0b7c64b
jannes <jannes.dev@posteo.net>
2019-06-03 23:02:57 +0200

    update build to tag images for dockerhub modify run scripts of the containers to resolve addresses correctly

--- wdm-microservices-project/build.sh
+++ wdm-microservices-project/build.sh
@@ -26,3 +26,10 @@ docker build -t wdm/orders orders/
 docker build -t wdm/config config-service/
 docker build -t wdm/eureka eureka/
 docker build -t wdm/gateway gateway/
+docker tag wdm/config jannest/wdm:config
+docker tag wdm/eureka jannest/wdm:eureka
+docker tag wdm/gateway jannest/wdm:gateway
+docker tag wdm/orders jannest/wdm:orders
+docker tag wdm/payments jannest/wdm:payments
+docker tag wdm/stocks jannest/wdm:stocks
+docker tag wdm/users jannest/wdm:users
--- wdm-microservices-project/eureka/run-in-docker.sh
+++ wdm-microservices-project/eureka/run-in-docker.sh
@@ -3,7 +3,11 @@
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port 8888"
 echo "********************************************************"
-while ! `nc -z config-service 8888`; do sleep 3; done
+# ugly ass hack
+config_uri_var=`echo $CONFIG_URI`
+config_uri_length=`echo ${#config_uri_var}`
+config_uri_no_port=`echo ${config_uri_var:7:config_uri_length-12}`
+while ! `nc -z "$config_uri_no_port" 8888`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
 
