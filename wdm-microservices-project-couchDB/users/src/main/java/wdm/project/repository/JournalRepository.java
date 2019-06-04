edb25f0077f76ab9071a4623d498287d94575de7
Panagiotis Soilis <pan.soilis@gmail.com>
2019-06-04 17:41:29 +0200

    JournalEntry View

--- wdm-microservices-project-couchDB/users/src/main/java/wdm/project/repository/JournalRepository.java
+++ wdm-microservices-project-couchDB/users/src/main/java/wdm/project/repository/JournalRepository.java
@@ -2,16 +2,24 @@ package wdm.project.repository;
 
 import org.ektorp.CouchDbConnector;
 import org.ektorp.support.CouchDbRepositorySupport;
+import org.ektorp.support.View;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import wdm.project.dto.JournalEntry;
 
+import java.util.List;
+
 @Component
+@View(name = "events", map = "function(doc) { if (doc.type == 'event' ) emit(doc._id, doc.status)}")
 public class JournalRepository extends CouchDbRepositorySupport<JournalEntry> {
 
 	@Autowired
 	public JournalRepository(CouchDbConnector db) {
 		super(JournalEntry.class, db);
 		initStandardDesignDocument();
+	}
+
+	public List<JournalEntry> findJournal(String id) {
+		return queryView("events", id);
 	}
 }
