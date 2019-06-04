3519e21d6777498faff959765f3a745b8d520d44
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-06-04 21:25:40 +0200

    Remove invalid mapping of Stock creation

--- wdm-microservices-project/stocks/src/main/java/wdm/project/endpoint/StocksEndpoint.java
+++ wdm-microservices-project/stocks/src/main/java/wdm/project/endpoint/StocksEndpoint.java
@@ -31,7 +31,7 @@ public class StocksEndpoint {
         return stocksService.getItemAvailability(itemId);
     }
 
-    @PostMapping("/item/create/")
+    @PostMapping("/item/create")
     public Long createItem(@RequestBody Item requestItem) {
         Item item = stocksService.createItem(requestItem);
         return item.getId();
