4d66467f0065e7bf3dc1439da4afc335e907a9aa
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-06-04 21:21:12 +0200

    Fix locust script and populate URL

--- wdm-microservices-project/stress-test/main.py
+++ wdm-microservices-project/stress-test/main.py
@@ -6,15 +6,30 @@ from locust import HttpLocust, TaskSet, task
 
 class User(TaskSet):
 
-    def setup(self):
-        create_response = self.client.post(
-            url=f"/api/orders/orders/create/{self.locust.user_id}")
-        self.order_id = create_response.json()
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.order_id = 0
+        self.user_ids = list()
+        self.item_ids = list()
 
-    @task(5)
+    def on_start(self):
+        with open("user_id.pickle", "rb") as user_id_file:
+            self.user_ids = pickle.load(user_id_file)
+            print(self.user_ids)
+        with open("item_id.pickle", "rb") as item_id_file:
+            self.item_ids = pickle.load(item_id_file)
+        self._get_new_order()
+
+    def _get_new_order(self):
+        create_response = self.client.post(
+            url=f"/api/orders/orders/create/{random.choice(self.user_ids)}")
+        if create_response.status_code == 200:
+            self.order_id = create_response.json()
+
+    @task(10)
     def add_item(self):
         response = self.client.post(
-            url=f"/api/orders/orders/addItem/{self.order_id}/{random.choice(self.locust.item_ids)}",
+            url=f"/api/orders/orders/addItem/{self.order_id}/{random.choice(self.item_ids)}",
             json={"amount": random.randint(1, 5)}
         )
         if response.status_code == 400:
@@ -35,15 +50,10 @@ class User(TaskSet):
         )
         if response.status_code == 400:
             response.success()
-        self.interrupt()
+        self._get_new_order()
 
 
 class WebsiteUser(HttpLocust):
-
-    def setup(self):
-        self.user_ids = pickle.load("user_id.pickle")
-        self.item_ids = pickle.load("item_id.pickle")
-
     task_set = User
     min_wait = 0
     max_wait = 1000
--- wdm-microservices-project/stress-test/populate/populate.py
+++ wdm-microservices-project/stress-test/populate/populate.py
@@ -10,6 +10,17 @@ with open(sys.argv[2], "rb") as user_file:
 with open(sys.argv[3], "rb") as item_file:
     items = pickle.load(item_file)
 
+item_ids = list()
+for item in items:
+    response = requests.post(url=f"{gateway_url}/api/stock/stock/item/create", json=item)
+    if response.status_code == 200:
+        item_ids.append(response.json())
+    else:
+        raise ConnectionError(f"Creation of item {item} has failed: {response.status_code}")
+with open(sys.argv[5], "wb") as item_id_file:
+    pickle.dump(item_ids, item_id_file)
+
+
 user_ids = list()
 for user in users:
     response = requests.post(url=f"{gateway_url}/api/users/users/create", json=user)
@@ -19,13 +30,3 @@ for user in users:
         raise ConnectionError(f"Creation of user {user} has failed: {response.status_code}")
 with open(sys.argv[4], "wb") as user_id_file:
     pickle.dump(user_ids, user_id_file)
-
-item_ids = list()
-for item in items:
-    response = requests.post(url=f"{gateway_url}/api/items/items/create", json=item)
-    if response.status_code == 200:
-        item_ids.append(response.json())
-    else:
-        raise ConnectionError(f"Creation of item {item} has failed.")
-with open(sys.argv[5], "wb") as item_id_file:
-    pickle.dump(item_ids, item_id_file)
