afbe0639059ec01f14bb6e218abef2b3132c1b25
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-06-04 18:41:27 +0200

    Add initial population script

---
+++ wdm-microservices-project/stress-test/populate/generate_population.py
@@ -0,0 +1,27 @@
+import pickle
+import random
+import sys
+
+num_users = int(sys.argv[1])
+num_items = int(sys.argv[2])
+min_credit = int(sys.argv[3])
+max_credit = int(sys.argv[4])
+min_stock = int(sys.argv[5])
+max_stock = int(sys.argv[6])
+
+users = [{"name": f"User{index}",
+          "credit": "%d" % random.randint(min_credit, max_stock)}
+         for index in range(num_users)]
+with open("users.pickle", "wb") as user_file:
+    pickle.dump(users, user_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+items = [{"title": f"Item{index}",
+          "stock": random.randint(min_stock, max_stock),
+          "price": 1}
+         for index in range(num_items)]
+
+with open("items.pickle", "wb") as item_file:
+    pickle.dump(items, item_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+print(users)
+print(items)
---
+++ wdm-microservices-project/stress-test/populate/populate.py
@@ -0,0 +1,31 @@
+import requests
+import pickle
+import sys
+
+gateway_url = sys.argv[1]
+
+with open(sys.argv[2], "rb") as user_file:
+    users = pickle.load(user_file)
+
+with open(sys.argv[3], "rb") as item_file:
+    items = pickle.load(item_file)
+
+user_ids = list()
+for user in users:
+    response = requests.post(url=f"{gateway_url}/api/users/users/create", json=user)
+    if response.status_code == 200:
+        user_ids.append(response.json())
+    else:
+        raise ConnectionError(f"Creation of user {user} has failed: {response.status_code}")
+with open(sys.argv[4], "wb") as user_id_file:
+    pickle.dump(user_ids, user_id_file)
+
+item_ids = list()
+for item in items:
+    response = requests.post(url=f"{gateway_url}/api/items/items/create", json=item)
+    if response.status_code == 200:
+        item_ids.append(response.json())
+    else:
+        raise ConnectionError(f"Creation of item {item} has failed.")
+with open(sys.argv[5], "wb") as item_id_file:
+    pickle.dump(item_ids, item_id_file)
