defa3b3cff7aad2a0e991660186b12ce7d22437b
jannes <jannes.dev@posteo.net>
2019-06-06 12:44:47 +0200

    Dockerfiles: install curl runscripts: echo DOCKER_HOST_IP

--- wdm-microservices-project/stocks/run-in-docker.sh
+++ wdm-microservices-project/stocks/run-in-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export DOCKER_HOST_IP=$(curl --retry 3 --connect-timeout 2 --max-time 2  -s 169.254.169.254/latest/meta-data/public-ipv4)
+echo "$DOCKER_HOST_IP"
 
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port 8888"
