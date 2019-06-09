0d2c3a2a4cf97906a1aab43b37a52660a447777f
agamvrinos <agg_gam@hotmail.com>
2019-06-09 12:15:57 +0200

    Update Locust stress tests

--- stress-test/locustio/helm/tasks/tasks.py
+++ stress-test/locustio/helm/tasks/tasks.py
@@ -1,7 +1,10 @@
 import pickle
 import random
-
+from google.cloud import storage
 from locust import HttpLocust, TaskSet, task, events
+
+client = storage.Client.from_service_account_json('locust/key.json')
+bucket = client.get_bucket('wdm_locust')
 
 
 class User(TaskSet):
@@ -11,19 +14,33 @@ class User(TaskSet):
         self.order_id = 0
         self.user_ids = list()
         self.item_ids = list()
+        self.outputBlob = None
 
     def on_start(self):
-        with open("user_id.pickle", "rb") as user_id_file:
-            self.user_ids = pickle.load(user_id_file)
-        with open("item_id.pickle", "rb") as item_id_file:
-            self.item_ids = pickle.load(item_id_file)
+        user_blob = bucket.get_blob('userid.pickle')
+        item_blob = bucket.get_blob('itemid.pickle')
+
+        user_id_pickle_cloud = open("userid.pickle", "wb")
+        user_blob.download_to_file(user_id_pickle_cloud)
+
+        item_id_pickle_cloud = open("itemid.pickle", "wb")
+        item_blob.download_to_file(item_id_pickle_cloud)
+
+        user_id_pickle_cloud.close()
+        item_id_pickle_cloud.close()
+
+        with open("userid.pickle", 'rb') as f:
+            self.user_ids = pickle.loads(f.read())
+        with open("itemid.pickle", 'rb') as f:
+            self.item_ids = pickle.loads(f.read())
+
         self._get_new_order()
 
     def _get_new_order(self, previous_id=0):
         while self.order_id == previous_id:
             with self.client.post(
-                    url=f"/api/orders/orders/create/{random.choice(self.user_ids)}",
-                    name="/api/orders/orders/create/{user_id}",
+                    url=f"/orders/create/{random.choice(self.user_ids)}",
+                    name="/orders/create/{user_id}",
                     catch_response=True) as create_response:
                 if create_response.status_code == 200:
                     self.order_id = create_response.json()
@@ -32,9 +49,9 @@ class User(TaskSet):
     @task(10)
     def add_item(self):
         with self.client.post(
-                url=f"/api/orders/orders/addItem/{self.order_id}/{random.choice(self.item_ids)}",
+                url=f"/orders/addItem/{self.order_id}/{random.choice(self.item_ids)}",
                 json={"amount": random.randint(1, 5)},
-                name="/api/orders/orders/addItem/{order_id}/{item_id}",
+                name="/orders/addItem/{order_id}/{item_id}",
                 catch_response=True) as response:
             if response.status_code == 400:
                 response.success()
@@ -42,8 +59,8 @@ class User(TaskSet):
     @task(1)
     def check_status(self):
         with self.client.get(
-                url=f"/api/orders/orders/find/{self.order_id}",
-                name="/api/orders/orders/find/{order_id}",
+                url=f"/orders/find/{self.order_id}",
+                name="/orders/find/{order_id}",
                 catch_response=True) as response:
             if response.status_code == 400:
                 response.success()
@@ -51,8 +68,8 @@ class User(TaskSet):
     @task(1)
     def checkout(self):
         with self.client.post(
-                url=f"/api/orders/orders/checkout/{self.order_id}",
-                name="/api/orders/orders/checkout/{order_id}",
+                url=f"/orders/checkout/{self.order_id}",
+                name="/orders/checkout/{order_id}",
                 catch_response=True) as response:
             if response.status_code == 400:
                 response.success()
@@ -61,8 +78,8 @@ class User(TaskSet):
 
 class WebsiteUser(HttpLocust):
     task_set = User
-    min_wait = 0
-    max_wait = 1000
+    min_wait = 500
+    max_wait = 500
 
 
 def locust_error_handler(locust_instance, exception, tb, **kwargs):
@@ -86,8 +103,16 @@ def slave_report_handler(client_id, data):
         failure_csv.write('\n'.join(failure_data) + ('\n' if failure_data else ''))
 
 
+def quitting_handler():
+    success_blob = bucket.blob('success.csv')
+    success_blob.upload_from_filename(filename='success.csv')
+    failure_blob = bucket.blob('failure.csv')
+    failure_blob.upload_from_filename(filename='failure.csv')
+
+
 events.locust_error += locust_error_handler
 events.slave_report += slave_report_handler
+events.quitting += quitting_handler
 
 success_filename = "success.csv"
 with open(success_filename, "w+") as success_csv:
