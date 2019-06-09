599f89a8b4a6ac0ffe734794c7bd41236f96c0cb
jannes <jannes.dev@posteo.net>
2019-06-09 14:45:27 +0200

    postgres: update services to register at eureka with acutal IP

--- wdm-microservices-project/orders/src/main/java/wdm/project/OrdersApplication.java
+++ wdm-microservices-project/orders/src/main/java/wdm/project/OrdersApplication.java
@@ -2,7 +2,11 @@ package wdm.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,5 +14,14 @@ public class OrdersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersApplication.class, args);
+	}
+
+	@Bean
+	@Profile("aws")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		b.setIpAddress(System.getenv("DOCKER_HOST_IP"));
+		b.setNonSecurePort(8080);
+		return b;
 	}
 }
--- wdm-microservices-project/users/src/main/java/wdm/project/UsersApplication.java
+++ wdm-microservices-project/users/src/main/java/wdm/project/UsersApplication.java
@@ -3,6 +3,10 @@ package wdm.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +14,14 @@ public class UsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
+	}
+
+	@Bean
+	@Profile("aws")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		b.setIpAddress(System.getenv("DOCKER_HOST_IP"));
+		b.setNonSecurePort(8083);
+		return b;
 	}
 }
