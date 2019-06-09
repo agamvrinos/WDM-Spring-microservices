ab2480d4a6a535847e701f550760565c6d2f3ffe
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-06-09 11:20:03 +0200

    CouchDB-proj: Fix external IP setting

--- wdm-microservices-project-couchDB/stocks/src/main/java/wdm/project/StocksApplication.java
+++ wdm-microservices-project-couchDB/stocks/src/main/java/wdm/project/StocksApplication.java
@@ -2,7 +2,11 @@ package wdm.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,4 +15,13 @@ public class StocksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StocksApplication.class, args);
 	}
+
+    @Bean
+    @Profile("aws")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        b.setIpAddress(System.getenv("DOCKER_HOST_IP"));
+        b.setNonSecurePort(8082);
+        return b;
+    }
 }
