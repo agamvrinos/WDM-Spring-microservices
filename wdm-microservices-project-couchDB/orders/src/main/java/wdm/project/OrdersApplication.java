a10df275f98c6710a5134b06fea06b7c5c787bea
Kyriakos Psarakis <kpsarakis94@gmail.com>
2019-06-09 19:57:13 +0200

    Update OrdersApplication.java

--- wdm-microservices-project-couchDB/orders/src/main/java/wdm/project/OrdersApplication.java
+++ wdm-microservices-project-couchDB/orders/src/main/java/wdm/project/OrdersApplication.java
@@ -2,7 +2,6 @@ package wdm.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableFeignClients
